f9c3a2527ef21d8ae175d3fdf26af8ada098cd1c
Jetsly <cnljli@live.com>
2014-10-26 22:45:05 +0800

    erwer

---
+++ Model/DBProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace WebApplication1
+{
+    public partial class DBModel<T> : IQueryable<T>
+    {
+        public DBModel()  
+        {  
+            Provider = new DbQueryProvider();  
+            Expression = Expression.Constant(this);//最后一个表达式将是第一IQueryable对象的引用。  
+        }  
+        public DBModel(Expression expression)  
+        {  
+            Provider = new DbQueryProvider();  
+            Expression = expression;  
+        }   
+
+        public Type ElementType
+        {
+            get { return typeof(T); }
+            private set { ElementType = value; }
+        }
+
+        public System.Linq.Expressions.Expression Expression
+        {
+            get;
+            private set;
+        }
+
+        public IQueryProvider Provider
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (Provider.Execute<T>(Expression) as IEnumerable<T>).GetEnumerator();
+        }
+
+        IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return (Provider.Execute(Expression) as IEnumerable).GetEnumerator();
+        }   
+  
+    }
+}
---
+++ Model/DBQueryProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class DbQueryProvider : IQueryProvider
+    {
+        public IQueryable<TElement> CreateQuery<TElement>(System.Linq.Expressions.Expression expression)
+        {
+            return new DBModel<TElement>();
+        }
+
+        public IQueryable CreateQuery(System.Linq.Expressions.Expression expression)
+        {
+            //这里牵扯到对表达式树的分析，就不多说了。  
+            throw new NotImplementedException();
+        }
+
+        public TResult Execute<TResult>(System.Linq.Expressions.Expression expression)
+        {
+            return default(TResult);//强类型数据集  
+        }
+
+        public object Execute(System.Linq.Expressions.Expression expression)
+        {
+            return new List<object>();//弱类型数据集  
+        }
+    }  
+}
